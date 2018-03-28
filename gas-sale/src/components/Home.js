59c50ae73aae0bc10d820a3c2dd8dbb90e278a74
MarcusBliss <biormarcus@gmail.com>
2018-03-28 13:42:53 -0400

    gas map and npm run build

--- gas-sale/src/components/Home.js
+++ gas-sale/src/components/Home.js
@@ -11,6 +11,26 @@ class Home extends Component {
   }
 
   render(){
+      const markers = [
+          {id:1, key:'1', defaultAnimation:2, label:'Total Gas Filling Station', position:{lat:6.5291228, lng:3.2507272}},
+          {id:2, key:'2', defaultAnimation:2, label:'Oando Gas Filling Station', position:{lat:6.5134438, lng:3.2475515}},
+          {id:3, key:'3', defaultAnimation:2, label:'NNPC Gas Filling Station', position:{lat:6.6090813, lng:3.2843326}},
+          {id:4, key:'4', defaultAnimation:2, label:'Oando Gas Filling Station', position:{lat:6.5307347, lng:3.2828577}},
+          {id:5, key:'5', defaultAnimation:2, label:'MRS Gas Filling Station', position:{lat:6.5124631, lng:3.2787711}},
+          {id:6, key:'6', defaultAnimation:2, label:'MRS Gas Filling Station', position:{lat:6.5101126, lng:3.291358}},
+          {id:7, key:'7', defaultAnimation:2, label:'Conoil Gas Filling Station', position:{lat:6.5075298, lng:3.2930413}},
+          {id:8, key:'8', defaultAnimation:2, label:'MRS Gas Filling Station', position:{lat:6.5062752, lng:3.3074624}},
+          {id:9, key:'9', defaultAnimation:2, label:'Oando Gas Filling Station', position:{lat:6.613641, lng:3.2583028}},
+          {id:10, key:'10', defaultAnimation:2, label:'Oando Gas Filling Station', position:{lat:6.5120571, lng:3.2892032}},
+          {id:11, key:'11', defaultAnimation:2, label:'Oando Gas Filling Station', position:{lat:6.4429131, lng:3.3516216}},
+          {id:12, key:'12', defaultAnimation:2, label:'Oando Gas Filling Station', position:{lat:6.5134438, lng:3.2475515}},
+          {id:13, key:'13', defaultAnimation:2, label:'Forte Gas Filling Station', position:{lat:6.4438992, lng:3.3566779}},
+          {id:14, key:'14', defaultAnimation:2, label:'NPPC Gas Filling Station Isheri', position:{lat:6.534994, lng:3.2353529}},
+          {id:15, key:'15', defaultAnimation:2, label:'Total Gas Filling Station', position:{lat:6.4538655, lng:3.3331237}},
+          {id:16, key:'16', defaultAnimation:2, label:'Mobile Gas Filling Station Apapa', position:{lat:6.5386857, lng:3.2847987}},
+
+          ]
+
     return (
         <div className="wrapper">
             <div className="sidebar" data-background-color="white" data-active-color="danger">
@@ -25,9 +45,9 @@ class Home extends Component {
                               map: map
                             })
                           }}
-
+                          markers={markers}
                           zoom={14}
-                          center={{lat:6.5243793, lng:3.3792057}}
+                          center={{lat:6.5291228, lng:3.2507272}}
                           containerElement={<div style={{height:100+'%'}} />}
                           mapElement={<div style={{height:100+'%'}} />} />
 
