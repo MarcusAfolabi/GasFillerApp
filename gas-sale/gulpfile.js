77283a04e024ba53407ec15aeabe974a5e85690e
MarcusBliss <biormarcus@gmail.com>
2018-03-28 05:41:15 -0400

    home.js and gulp sass

--- gas-sale/gulpfile.js
+++ gas-sale/gulpfile.js
@@ -5,11 +5,12 @@ var autoprefixer = require('gulp-autoprefixer')
 var gp_concat = require('gulp-concat')
 var gp_rename = require('gulp-rename')
 var gp_uglify = require('gulp-uglify')
+var sass = require('gulp-sass')
 // var less = require('gulp-less')
 var to5 = require('gulp-6to5')
 var path = require('path')
 
-gulp.task('css', function(){
+gulp.task('css', ['sass'], function(){
     return gulp.src(
             [
                 './public/css/bootstrap.min.css',
@@ -22,6 +23,12 @@ gulp.task('css', function(){
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
         .pipe(gp_concat('style.min.css'))
         .pipe(gulp.dest('./public/dist/css/'))
+})
+
+gulp.task('sass', function(){
+  return gulp.src('./public/sass/paper-dashboard.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./public/css'))
 })
 
 gulp.task('copy-fonts', function(){
